abf8982f2779cc941288061426b95b2efa9953bb
Bhashitha Arambepola <barambepola@gmail.com>
2022-06-09 19:47:30 +0530

    finalize login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DashBoardComponent} from "./dash-board/dash-board.component";
+import {LoginComponent} from "./login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashBoard',
+    component: DashBoardComponent
+  },{
+  path:'login',
+    component: LoginComponent
+  },
+  {
+    path: "",
+    pathMatch: 'full',
+    redirectTo: 'login'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {UserService} from "../service/user.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  name = "";
+  valid = false;
+  @ViewChild("txtName")
+  txtNameWrapper!:ElementRef<HTMLInputElement>;
+
+  constructor(private userService: UserService,
+private routerService:Router){
+
+
   }
 
+  ngOnInit(): void {
+   if( this.userService.getPrincipal()){
+     this.routerService.navigateByUrl('/dashBoard')
+   }
+  }
+
+  enter(): void {
+    if (this.userService.login(this.name)) {
+this.valid=false;
+this.routerService.navigateByUrl('/dashBoard');
+
+    } else {
+     this.valid=true
+      this.txtNameWrapper.nativeElement.select();
+     this.txtNameWrapper.nativeElement.focus();
+    }
+  }
 }
